9d56d3a49b69e0312f0823e007c1cdd908099004
kxrn0 <kxrp@protonmail.com>
2022-03-03 17:53:35 -0600

    test

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function map(value, start1, end1, start2, end2) {
     return start2 + (end2 - start2) * (value - start1) / (end1 - start1);
 }
 
-alert("hi");
+alert(window.innerWidth);
 
 let clockObj = (
     () => {
